35c2bc081f44ee9873d73a9134b53614d69520df
William <william.thal.67@gmail.com>
2021-06-22 16:29:38 +0200

    created localStorage for cart and started function to add item to cart

--- js/product.js
+++ js/product.js
@@ -81,38 +81,22 @@ const makeForm = function(optionList){
 displayItem();
 
 
+//cart array init if it doesn't exist
+let checkCart = localStorage.getItem("cart");
+if(checkCart === null){
+    var cart = [];
+    window.localStorage;
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+
 //CODE FOR THE ADD TO CART BUTTON
-const addToCart = async function(){
-//Test the /order api
-    let order = {
-        contact: {
-            firstName:" ", 
-            lastName:" ", 
-            address:" ", 
-            city:" ", 
-            email:" "
-        },
-        products: [productId]
-    };
+const addToCart = function(){
 
-    console.log(order);
-    
-    let response = await fetch("http://localhost:3000/api/cameras/order",{
-        method: "POST",
-        headers: {
-            "Accept": 'application/json', 
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(order)
-    })
-    if(response.ok){
-        let data = await response.json();
-        console.log(data.products);
-    }else{
-        console.log(response);
-    }
-    
-
-    
+    let cart = localStorage.getItem("cart");
+    cart = JSON.parse(cart);
+    cart.push(productId);
+    console.log(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
 
 }
